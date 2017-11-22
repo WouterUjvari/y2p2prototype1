76d6110ebe3204b628f12931b1def7aec1274380
GMD1A_WouterUjvari <wouter.ujvari@gmail.com>
2017-11-22 21:13:03 +0100

    post

---
+++ Assets/Scripts/FireArm.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FireArm : MonoBehaviour
+{
+
+    public GameObject projectile;
+    public Transform muzzle;
+    public float force;
+
+    void Update()
+    {
+        Shoot();
+    }
+
+    void Shoot()
+    {
+        if(Input.GetButtonDown("Fire1"))
+        {
+            GameObject g = Instantiate(projectile, muzzle);
+            g.transform.SetParent(null);
+            g.GetComponent<Rigidbody>().AddForce(transform.forward * force);
+
+
+        }
+    }
+}
